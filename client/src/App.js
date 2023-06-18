468cfdf76b35498935a64f6db7f37596d632647a
root <root@THEMACHINE.localdomain>
2023-06-18 14:16:40 +0800

    updated file

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello There !
+            Hello There ! 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
